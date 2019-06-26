b94560591bb4c6711e02596b66894172be51afb0
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-26 01:10:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,11 +26,17 @@ function add2(n) {
 //   }
 // }
 
+
 var funkyFunction  =function() {
- 
+   function() {
     return "FUNKY!";
- 
+  }
 }
+
+
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
+var theFunk = funkyFunction;
 
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
